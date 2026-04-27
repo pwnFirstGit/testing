2c15d3101caa15635edd85c5eed96515fbff8371
pwnFirstGit <pawanforyou18@gmail.com>
2026-04-27 08:00:51 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,8 @@
 def factorial(n):
-    
     if n == 0:  
         return 0  
-
     return n * factorial(n - 1)
 
-
-# Test the faulty function
 try:
     num = 5
     print(f"Factorial of {num} is: {factorial(num)}")
